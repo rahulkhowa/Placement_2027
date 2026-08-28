40699c7e3e48daa5b84bb0bc83f27427fd12b5d1
Rahul Kumawat <rahulkhowal0803@gmail.com>
2026-08-28 16:09:35 +0530

    Time: 10 ms (81.65%) | Memory: 78 MB (74.68%) - LeetSync

--- 2686-minimum-cost-of-a-path-with-special-roads/minimum-cost-of-a-path-with-special-roads.cpp
+++ 2686-minimum-cost-of-a-path-with-special-roads/minimum-cost-of-a-path-with-special-roads.cpp
@@ -1,67 +1,34 @@
 class Solution {
 public:
     int minimumCost(vector<int>& start, vector<int>& target, vector<vector<int>>& specialRoads) {
-        map<pair<int,int>,vector<vector<int>>>adj;
-        int n = specialRoads.size();
-        set<pair<int,int>>st;
-        for(int i=0;i<n;i++){
-            int x1 = specialRoads[i][0];
-            int y1 = specialRoads[i][1];
-            int x2 = specialRoads[i][2];
-            int y2 = specialRoads[i][3];
-            int c = specialRoads[i][4];
-            st.insert({x1,y1});
-            st.insert({x2,y2});
-        }
-        vector<pair<int,int>>edge1;
-        for(auto it:st){
-            edge1.push_back(it);
-        }
-        int m = edge1.size();
+        int m = specialRoads.size();
+        vector<pair<int,int>>pos(m+2);
+        pos[0]={start[0],start[1]};
         for(int i=0;i<m;i++){
-            for(int j=0;j<m;j++){
-                if(i!=j){
-                    vector<int>tmp;
-                    tmp.push_back(edge1[j].first);
-                    tmp.push_back(edge1[j].second);
-                    tmp.push_back(abs(edge1[i].first-edge1[j].first)+abs(edge1[i].second-edge1[j].second));
-                    adj[edge1[i]].push_back(tmp);
-                }
-            }
+            pos[i+1]={specialRoads[i][2],specialRoads[i][3]};
         }
-        for(int i=0;i<n;i++){
-            int x1 = specialRoads[i][0];
-            int y1 = specialRoads[i][1];
-            int x2 = specialRoads[i][2];
-            int y2 = specialRoads[i][3];
-            int c = specialRoads[i][4];
-            pair<int,int>s = {start[0],start[1]};
-            pair<int,int>t = {target[0],target[1]};
-            adj[s].push_back({x1,y1,abs(s.first-x1)+abs(s.second-y1)});
-            adj[s].push_back({x2,y2,abs(s.first-x2)+abs(s.second-y2)});
-            adj[{x1,y1}].push_back({t.first,t.second,abs(x1-t.first)+abs(y1-t.second)});
-            adj[{x2,y2}].push_back({t.first,t.second,abs(x2-t.first)+abs(y2-t.second)});
-            adj[{x1,y1}].push_back({x2,y2,c});
-        }
-        map<pair<int,int>,int>dp;
-        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>q;
-        q.push({0,start[0],start[1]});
-        dp[{start[0],start[1]}]=0;
+        pos[m+1] = {target[0],target[1]}; 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>q;
+        q.push({0,0});
+        vector<int>dp(m+2,INT_MAX);//nodes toh m+2 hi hai edges boht sari ho sakte hai;
+        dp[0]=0;
         while(!q.empty()){
-            auto [d,i,j] = q.top();q.pop();
-            cout<<i<<" "<<j<<endl;
-            if(i==target[0] && j==target[1]) return d;
-            if(!dp.count({i,j}) || dp[{i,j}]!=d) continue;
-            for(auto e:adj[{i,j}]){
-                pair<int,int>v = {e[0],e[1]};
-                // cout<<v.first<<" "<<v.second<<endl;
-                int w = e[2];
-                if(!dp.count(v) || dp[v] > d + w){
-                    dp[v]=d+w;
-                    q.push({dp[v],v.first,v.second});
-                }
-            }
+           auto [d,u] = q.top();q.pop();
+           if(dp[u]!=d) continue;
+           if(u==m+1) return d;
+           int cost1 = abs(pos[u].first-pos[m+1].first) + abs(pos[u].second-pos[m+1].second);
+           if(dp[m+1]>d+cost1){
+              dp[m+1] = d+cost1;
+              q.push({dp[m+1],m+1});
+           }
+           for(int i=0;i<m;i++){
+              int cost2 = abs(pos[u].first-specialRoads[i][0]) + abs(pos[u].second-specialRoads[i][1]) + specialRoads[i][4];
+              if(dp[i+1] > d+cost2){
+                dp[i+1]=d+cost2;
+                q.push({dp[i+1],i+1});
+              }
+           }
         }
-        return abs(start[0]-target[0]) + abs(start[1]-target[1]);
+        return dp[m+1];
     }
 };
