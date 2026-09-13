05da849c58a96c89ff9c4566c265b6b4d380fcff
Rahul Kumawat <rahulkhowal0803@gmail.com>
2026-09-13 15:42:02 +0530

    Time: 5 ms (98.47%) | Memory: 9.4 MB (42.51%) - LeetSync

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,51 +1,43 @@
 class Solution {
 public:
-    int lps(string s) {
-        int n = s.size();
-        if(n==0) return 0;
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++) dp[i][i]=1;
-        for(int len=2;len<=n;len++){
-            for(int i=0;i+len-1<n;i++){
-                int j = i+len-1;
-                if(s[i]==s[j]){
-                    dp[i][j]=max(dp[i][j],dp[i+1][j-1]+2);
-                }
-                else{
-                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
-                }
-            }
-        }
-        return dp[0][n-1];
-    }
-    bool isPal(string &s){
-    int l=0,r=s.size()-1;
-    while(l<=r){
-        if(s[l]!=s[r]) return false;
-        l++;
-        r--;
-    }
-    return true;
-}
     int maxProduct(string s) {
-        int n = s.size();
-        int N = 1<<n;
-        int ans=0;
-        for(int mask=0;mask<N;mask++){
-            string a = "",b="";
+        int n=s.size();
+        int N=1<<n;
+
+        vector<int> dp(N,0);
+
+        for(int mask=1;mask<N;mask++){
+            int l=-1,r=-1;
+
             for(int i=0;i<n;i++){
-                if(!(mask&(1<<i))){
-                    b+=s[i];
-                }
-                else{
-                    a+=s[i];
+                if(mask&(1<<i)){
+                    if(l==-1) l=i;
+                    r=i;
                 }
             }
-            if(!isPal(a)) continue;
-            int len1 = a.size();
-            int len2 = lps(b);
-            ans=max(ans,len1*len2);
+
+            if(l==r){
+                dp[mask]=1;
+            }
+            else if(s[l]==s[r]){
+                int inside=mask^(1<<l)^(1<<r);
+                dp[mask]=dp[inside]+2;
+            }
+            else{
+                dp[mask]=max(
+                    dp[mask^(1<<l)],
+                    dp[mask^(1<<r)]
+                );
+            }
         }
+
+        int ans=0;
+
+        for(int mask=0;mask<N;mask++){
+            int other=(N-1)^mask;
+            ans=max(ans,dp[mask]*dp[other]);
+        }
+
         return ans;
     }
 };
