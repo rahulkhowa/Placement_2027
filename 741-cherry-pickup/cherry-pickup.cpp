0c6a656b7985e89f7442183376f4167aa793a6df
Rahul Kumawat <rahulkhowal0803@gmail.com>
2026-09-18 02:13:30 +0530

    Time: 47 ms (54.55%) | Memory: 40.9 MB (28.31%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,78 +1,57 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        // consider reverse path also as a forward path
-        // then problem becomes simultaneous two robots
-        // r1+c1 == r2+c2 (at each step)
-
         int n = grid.size();
 
-        // dp[r1][c1][c2] =
-        // max cherries collected by two robots
-        // P1 = (r1,c1)
-        // P2 = (r2,c2)
-        // r2 = r1+c1-c2
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
 
-        vector<vector<vector<int>>> dp(
-            n,
-            vector<vector<int>>(n, vector<int>(n, INT_MIN))
-        );
+        dp[0][0] = grid[0][0];
 
-        dp[0][0][0] = grid[0][0];
+        for(int k = 1; k <= 2*n-2; k++) {
 
-        for(int r1 = 0; r1 < n; r1++) {
-            for(int c1 = 0; c1 < n; c1++) {
+            vector<vector<int>> ndp(n, vector<int>(n, INT_MIN));
 
-                for(int c2 = 0; c2 < n; c2++) {
+            for(int r1 = 0; r1 < n; r1++) {
+                int c1 = k-r1;
 
-                    int r2 = r1 + c1 - c2;
+                if(c1 < 0 || c1 >= n || grid[r1][c1] == -1)
+                    continue;
 
-                    if(r2 < 0 || r2 >= n)
-                        continue;
+                for(int r2 = 0; r2 < n; r2++) {
+                    int c2 = k-r2;
 
-                    if(grid[r1][c1] == -1 || grid[r2][c2] == -1)
-                        continue;
-
-                    int one = INT_MIN;
-                    int two = INT_MIN;
-                    int three = INT_MIN;
-                    int four = INT_MIN;
-
-                    // P1 came from up
-                    if(r1 - 1 >= 0) {
-                        one = dp[r1-1][c1][c2];
-
-                        // P1 came from up, P2 came from left
-                        if(c2 - 1 >= 0) {
-                            three = dp[r1-1][c1][c2-1];
-                        }
-                    }
-
-                    // P1 came from left
-                    if(c1 - 1 >= 0) {
-                        two = dp[r1][c1-1][c2];
-
-                        // P1 came from left, P2 came from left
-                        if(c2 - 1 >= 0) {
-                            four = dp[r1][c1-1][c2-1];
-                        }
-                    }
-
-                    int f = max({one, two, three, four});
-
-                    if(f == INT_MIN)
+                    if(c2 < 0 || c2 >= n || grid[r2][c2] == -1)
                         continue;
 
                     int cherries = grid[r1][c1];
 
-                    if(r1 != r2 || c1 != c2)
+                    if(r1 != r2)
                         cherries += grid[r2][c2];
 
-                    dp[r1][c1][c2] = f + cherries;
+                    for(int dr1 : {0,1}) {
+                        for(int dr2 : {0,1}) {
+
+                            int pr1 = r1-dr1;
+                            int pr2 = r2-dr2;
+
+                            if(pr1 < 0 || pr2 < 0)
+                                continue;
+
+                            if(dp[pr1][pr2] == INT_MIN)
+                                continue;
+
+                            ndp[r1][r2] = max(
+                                ndp[r1][r2],
+                                dp[pr1][pr2] + cherries
+                            );
+                        }
+                    }
                 }
             }
+
+            dp = ndp;
         }
 
-        return max(0, dp[n-1][n-1][n-1]);
+        return max(0, dp[n-1][n-1]);
     }
 };
