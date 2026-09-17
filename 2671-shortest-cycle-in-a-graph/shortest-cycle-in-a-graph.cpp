853f47b2a639f126f848b2cbb50288951a5efbb8
Rahul Kumawat <rahulkhowal0803@gmail.com>
2026-09-18 01:26:16 +0530

    Time: 235 ms (61.04%) | Memory: 158.3 MB (38.36%) - LeetSync

--- 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int findShortestCycle(int n, vector<vector<int>>& edges) {
         vector<vector<int>>adj(n);
-        for(auto& e:edges){
-            int u=e[0];
-            int v=e[1];
+        for(auto e:edges){
+            int u = e[0];
+            int v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int mini=1e9;
+        int ans = n+1;
         for(int i=0;i<n;i++){
-            vector<int>dist(n,-1);
-            vector<int>par(n,-1);
             queue<int>q;
-            dist[i]=1;
             q.push(i);
+            vector<int>dist(n,-1),par(n,-1);
+            dist[i]=1;
             while(!q.empty()){
-                int node=q.front();
-                q.pop();
-                for(auto& padosi:adj[node]){
-                    if(dist[padosi]==-1){
-                        dist[padosi]=dist[node]+1;
-                        par[padosi]=node;
-                        q.push(padosi);
-                    }else if(padosi!=par[node]){
-                        mini=min(mini,dist[padosi]+dist[node]-1);
-                    }
+                auto u = q.front();q.pop();
+                for(int v:adj[u]){
+                   if(dist[v]==-1){
+                      dist[v]=dist[u]+1;
+                      par[v]=u;
+                      q.push(v);
+                   }
+                   else if(v!=par[u]){
+                     ans=min(ans,dist[u]+dist[v]-1);
+                   }
                 }
-
             }
         }
-        return (mini==1e9)?-1:mini;
+        return ans==n+1?-1:ans;
     }
 };
